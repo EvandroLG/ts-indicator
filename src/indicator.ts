f312ac0cd62ab9516f8fff630429370d9d4016c5
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2021-01-21 21:50:10 +0100

    animate progressbar

--- src/indicator.ts
+++ src/indicator.ts
@@ -7,13 +7,15 @@ type IndicatorType = {
 
 const indicator = ({ element, color }: IndicatorType = {}) => {
   const progress = createDiv(`
-    position: fixed;
+    position: sticky;
     top: 0;
     left: 0;
-    border-bottom: 2px solid ${color ?? 'blue'};
+    border-top: 1vh solid ${color ?? 'blue'};
+    transition: width 0.4s ease-out;
+    width: 0%;
   `);
 
-  document.body.appendChild(progress);
+  document.body.prepend(progress);
 
   const domElement = getElement(element ?? document.body);
 
@@ -24,7 +26,7 @@ const indicator = ({ element, color }: IndicatorType = {}) => {
   const onScroll = throttle(() => {
     const max = domElement.scrollHeight - window.innerHeight;
     progress.style.width = `${(window.pageYOffset / max) * 100}%`;
-  }, 100);
+  }, 300);
 
   window.addEventListener('scroll', onScroll);
 };
